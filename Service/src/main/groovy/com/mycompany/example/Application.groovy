dd6c64c73597d842990311fc2dd2f349139c97c7
Mohsin Khatib <mohsin.khatib@reachlocal.com>
2015-11-16 16:09:08 -0800

    Test project to reproduce spingboot gradle plugin issue in 1.3.0

---
+++ Service/src/main/groovy/com/mycompany/example/Application.groovy
@@ -0,0 +1,16 @@
+package com.mycompany.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration, HibernateJpaAutoConfiguration])
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run Application, args
+
+    }
+
+}
